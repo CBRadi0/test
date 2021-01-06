5373f9230f6198670a9b72507cd5b78f1146d67a
CBRadi0 <77046464+CBRadi0@users.noreply.github.com>
2021-01-06 20:20:08 +1100

    Deleted White Space

--- tims_house.py
+++ tims_house.py
@@ -1,8 +1,4 @@
 solution=lambda t:-(-t//5)
-
-
-
-
 # def solution(distance:int):
 #     while distance != 0:
 #         possible_strides = [5,4,3,2,1]
@@ -12,5 +8,4 @@ solution=lambda t:-(-t//5)
 #                 distance -= step
 #                 steps +=1
 #     return steps
-
 print(solution(32))
