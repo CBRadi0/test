233bd2856b92a978d7070075637b1278c8ab7923
CBRadi0 <77046464+CBRadi0@users.noreply.github.com>
2021-01-06 20:17:43 +1100

    Added tims_house.py

---
+++ tims_house.py
@@ -0,0 +1,16 @@
+solution=lambda t:-(-t//5)
+
+
+
+
+# def solution(distance:int):
+#     while distance != 0:
+#         possible_strides = [5,4,3,2,1]
+#         steps = 0
+#         for step in possible_strides:
+#             while distance >= step:
+#                 distance -= step
+#                 steps +=1
+#     return steps
+
+print(solution(32))
